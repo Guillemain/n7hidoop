1282d81ec4d0555f9c21c36b48b37bba6498b51b
Canin Amelie <acanin@localhost.localdomain>
2019-01-11 13:19:19 +0100

    Format

---
+++ src/formats/FormatLine.java
@@ -0,0 +1,127 @@
+package formats;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class FormatLine implements Format {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private FileReader fichierLecture;
+	private BufferedReader buffer;
+	private FileWriter fichierEcriture;
+    private String nameF;
+    private ArrayList<String> listeLigne = new ArrayList<>();
+
+	// Ici \|/ ****************************************************
+    private int index = 0; // --> ATTENTION ON COMMENCE A ZERO !!! <--  COMMENTAIRE POUR MARIE
+    //  Là /|\ ****************************************************
+    
+    // Gestion des threads : 
+    boolean oLect = false; boolean oEcriture = false;
+    
+    public FormatLine(String name){
+        nameF = name;
+    }
+
+	@Override
+	public KV read() {
+		if(!oLect){
+			System.err.println("Opértation interdite");
+			return null;
+		}
+		
+		if(index <= listeLigne.size()){
+			return null;
+		}
+		
+		KV retour = new KV();
+		retour =  new KV(Integer.toString(index + 1), listeLigne.get(index));
+		index++;
+		return retour;
+	}
+
+	@Override
+	public void write(KV record) {
+		if (!oEcriture) {
+			System.err.println("Opértation interdite");
+			return;
+		}
+		try {
+			fichierEcriture.write(record.v + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void open(OpenMode mode) {
+		try {
+            File fichier = new File(nameF);
+            if (mode == OpenMode.R){
+            	System.out.print("Ouverture du fichier " + nameF + " en mode lecture -> ");
+            	oLect = true;
+                //On ouvre le fichie en lecture
+                fichier.setReadable(true);
+                fichierLecture = new FileReader(fichier);
+                buffer = new BufferedReader(fichierLecture);
+                String ligne;
+                // On lit le contenu du fichier.
+                while ((ligne = buffer.readLine())!= null){ // On charge tout la rame /!\  ATTENTION FICHIER LOURD
+                	listeLigne.add(ligne);
+                }
+                buffer.close();
+                System.out.println("Lecture des lignes faites");
+            } else {
+            	System.out.println("Ouverture du fichier "+ nameF + "en ecriture.");
+            	oEcriture = true;
+            	fichier.setWritable(true);
+            	fichierEcriture = new FileWriter(fichier, true);            	
+            }
+        } catch (Exception e) {
+            System.err.println("Impossible d'ouvrir le fichier.");
+            e.printStackTrace();
+        }
+	}
+
+	@Override
+	public void close() {
+		try {
+			if(oEcriture){
+				fichierEcriture.close();
+				oEcriture = false;
+			}
+			if(oLect){
+				fichierLecture.close();
+				oLect = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public long getIndex() {
+		return index;
+	}
+
+	@Override
+	public String getFname() {
+		return nameF;
+	}
+
+	@Override
+	public void setFname(String fname) {
+		this.nameF = fname;
+
+	}
+
+}
